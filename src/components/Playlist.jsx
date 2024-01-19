ac37bee972a29148d6c0f1d00568b4b6c0c36622
EthanTransier <110576660+EthanTransier@users.noreply.github.com>
2024-01-18 23:59:35 -0700

    General Update

--- src/components/Playlist.jsx
+++ src/components/Playlist.jsx
@@ -4,7 +4,7 @@ import {useEffect} from 'react'
 const Playlist = () => {
     const playlistId = '5J3beXtGHeF7BFTMcdlRpD?si=b14c454d157240df'
   return (
-    <iframe style={{borderRadius: '12px', width: '85vw', height: '300px'}} src="https://open.spotify.com/embed/playlist/5J3beXtGHeF7BFTMcdlRpD?utm_source=generator" width="100%" height="300" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
+    <iframe style={{borderRadius: '12px', width: '85vw'}} src="https://open.spotify.com/embed/playlist/1pMb9t8DXhWMxOCMbI0uc9?utm_source=generator&theme=0" width="100%" height="200" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
   )
 }
 
